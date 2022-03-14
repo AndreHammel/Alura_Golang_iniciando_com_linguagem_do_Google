f807995761dfd5603186e082b1a18ee18b2caa8a
AndreHammel <hammel.engineer@gmail.com>
2022-03-14 19:57:26 -0300

    leitura de arquivo

--- src/hello.go
+++ src/hello.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -11,7 +14,6 @@ const MONITORAMENTO = 5
 const DELAY = 3
 
 func main() {
-
 	for {
 		exibeIntroducao()
 		exibeMenu()
@@ -29,6 +31,34 @@ func main() {
 				fmt.Println("Não conheço esse comando")
 				os.Exit(-1) 
 		}
+	}
+}
+
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitoriando ...")
+	sites := leSitesDoArquivo()
+
+	for i:=0; i < MONITORAMENTO; i++ {
+		time.Sleep(DELAY * time.Second)
+		for i, site := range sites{
+			fmt.Println("Testando site", i, ":", site )
+			testaSite(site)
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func testaSite(site string) {
+	resp, err := http.Get(site)
+	if err != nil {
+		fmt.Println("Ocorreu um erro na requisição:", err)
+	}
+	if resp.StatusCode == 200 {
+		fmt.Println("Site", site, "foi carregado com sucesso")
+	} else {
+		fmt.Println("Site: ", site, "está fora do ar. Status Code: ", resp.StatusCode)
 	}
 }
 
@@ -51,31 +81,24 @@ func exibeMenu() {
 	fmt.Println("0 - Sair do Programa")
 }
 
-func iniciarMonitoramento() {
-	fmt.Println("Monitoriando ...")
-	sites := []string{
-		"https://random-status-code.herokuapp.com",
-		"http://www.alura.com.br",
-		"https://www.caelum.com.br"}
+func leSitesDoArquivo() ([]string) {
 
-	for i:=0; i < MONITORAMENTO; i++ {
-		time.Sleep(DELAY * time.Second)
-		for i, site := range sites{
-			fmt.Println("Testando site", i, ":", site )
-			testaSite(site)
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+	
+	leitor := bufio.NewReader(arquivo)
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		sites = append(sites, linha)
+		if err == io.EOF {
+			break
 		}
-		fmt.Println()
 	}
-	fmt.Println()
+	arquivo.Close()
+	return sites
 }
-
-func testaSite(site string) {
-	resp, _ := http.Get(site)
-	if resp.StatusCode == 200 {
-		fmt.Println("Site", site, "foi carregado com sucesso")
-	} else {
-		fmt.Println("Site: ", site, "está fora do ar. Status Code: ", resp.StatusCode)
-	}
-}
-
-
